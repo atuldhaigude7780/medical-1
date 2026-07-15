d3434307d58f473c37eea36984f6a213078bbf53
atuldhaigude779 <atuldhaigude779@gmail.com>
2026-07-16 01:23:02 +0530

    commit 3

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,6 +5,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 	System.out.println("Demo Text-1");
+	System.out.println("Demo Text-2");
     }
-    
+
 }
